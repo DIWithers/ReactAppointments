61b7cf69cb21da1c4b20cb4d833e972b446d5419
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-09-07 16:01:56 -0400

    cookie auth

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-require('./services/passport');
-require('./models/User');
 const express = require('express');
 const app = express();
-
 const authRoutes = require('./routes/authRoutes');
-authRoutes(app);
-
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 const mongoose = require('mongoose');
+
+authRoutes(app);
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+)
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect(keys.mongoURI, {useNewUrlParser: true});
 
- const PORT = process.env.PORT || 5000;
- app.listen(PORT);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
