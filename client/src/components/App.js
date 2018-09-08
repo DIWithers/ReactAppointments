dfe85e8f897dc168cae6d8f9678af3dd72af5d51
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-09-08 16:03:01 -0400

    initial routing logic

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Appointments = () => <h2>Appointments</h2>;
+const AppointmentNew = () => <h2>AppointmentNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+const Clients = () => <h2>Clients</h2>
+const ClientNew = () => <h2>ClientNew</h2>
 
 const App = () => {
     return (
-        <div>Hi There!</div>
+        <div>
+            <Header />
+            <BrowserRouter>
+                <div>
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/appointments" component={Appointments} />
+                    <Route exact path="/appointments/new" component={AppointmentNew} />
+                    <Route exact path="/clients" component={Clients} />
+                    <Route exact path="/clients/new" component={ClientNew} />
+                </div>
+            </BrowserRouter>
+        </div>
     );
 };
 
