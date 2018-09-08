0a1b650f9332cc06e8935b3ffc8ba154e8bea252
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-09-07 21:56:53 -0400

    enable cookies

--- index.js
+++ index.js
@@ -3,10 +3,23 @@ require('./services/passport');
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 const authRoutes = require('./routes/authRoutes');
 authRoutes(app);
 
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const userSchema = new Schema({
-    googleID: String
+    googleId: String
 });
  mongoose.model('users', userSchema);
