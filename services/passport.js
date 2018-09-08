933fe4562360058b471566fe772045f1158a70d7
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-09-07 21:37:50 -0400

    check for existing user before creating new one

--- services/passport.js
+++ services/passport.js
@@ -11,6 +11,16 @@ const User = mongoose.model('users');
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        new User({ googleId: profile.id }).save();
+        User.findOne({ googleId: profile.id} )
+            .then((existingUser) => {
+                if (existingUser) {
+                    done(null, existingUser);
+                }
+                else {
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then( user => done(null, user));
+                }
+            });
     })
 ); 
