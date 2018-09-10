a3067f3eb172489634eb68185963e65fbffc4c3f
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-09-10 16:42:32 -0400

    can sign in and out

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import { faSignInAlt, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 
 class Header extends Component {
+renderContent() {
+    switch (this.props.auth) {
+        case null:
+            return;
+        case false:
+            return (
+                <div className="navbar-item">
+                    <a href="/auth/google" className="button is-danger">
+                        <span className="icon">
+                            <FontAwesomeIcon icon={faSignInAlt} />
+                        </span>
+                        <span>Login With Google</span>
+                    </a>
+                </div>
+            )
+        default:
+            return (
+                <div className="navbar-item">
+                    <a href="/api/logout" className="button is-danger">
+                        <span className="icon">
+                            <FontAwesomeIcon icon={faSignOutAlt} />
+                        </span>
+                        <span>Logout</span>
+                    </a>
+                </div>
+            )
+    }
+}
+
     render() {
         return (
             <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation">
@@ -18,19 +48,15 @@ class Header extends Component {
                 </div>
                 <div className="navbar-menu">
                     <div className="navbar-end">
-                    <div className="navbar-item">
-                        <a href="/auth/google" className="button is-danger">
-                            <span className="icon">
-                                <FontAwesomeIcon icon={faSignInAlt} />
-                            </span>
-                            <span>Login With Google</span>
-                        </a>
-                    </div>
+                        {this.renderContent()}
                     </div>
                 </div>
             </nav>
         );
     }
 }
+function mapStateToProps({ auth }) {
+    return { auth };
+}
 
-export default Header;
+export default connect(mapStateToProps)(Header);
