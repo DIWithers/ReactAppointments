b5700bff939b33cc1808d0b04fee263fa27db2a8
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-09-14 21:33:14 -0400

    slotField now a class with starter options programatically generated

--- client/src/components/appointments/SlotField.js
+++ client/src/components/appointments/SlotField.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default ({ input, label, meta: { error, touched } }) => {
-    return (
-        <div>
-            <label>{label}</label>
-            <select {...input} >
-                <option>First, pick a day.</option>
-                <option value="1">1PM</option>
-                <option value="2">2PM</option>
-                <option value="3">3PM</option>
-            </select>
-            {touched && error}
-        </div>
-    );
-};
+class SlotField extends Component {
+    render() {
+        const slots = [
+                 {
+                    military: 0,
+                    standard: "12:00 AM"
+                },
+                {
+                    military: 30,
+                    standard: "12:30 AM"
+                },
+                {
+                    military: 100,
+                    standard: "01:00 AM"
+                },
+                {
+                    military: 130,
+                    standard: "01:30 AM"
+                }
+            ]
+        let slotOptions =  slots.map((slot) =>
+            <option key={slot.military}>{slot.standard}</option>
+        );
+        return(
+            <div>
+                <label>{this.props.label}</label>
+                <select {...this.props.input}>
+                    {slotOptions}
+                </select>
+            </div> 
+        )
+    }
+}
+export default SlotField;
