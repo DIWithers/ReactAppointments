1dcf89eb8852a6a67be2816fa0739a6f0470ef54
Danielle Ivy Withers <d.ivy.withers@gmail.com>
2018-09-18 21:45:35 -0400

    can now fully save appt with start time part deux

--- routes/appointmentRoutes.js
+++ routes/appointmentRoutes.js
@@ -5,18 +5,27 @@ const Appointment = mongoose.model('appointments');
 
 module.exports = (app) => {
     app.post('/api/appointments', requireLogin, async (req, res) => {
-        const { name, email, phone, slot } = req.body;
+        const { name, email, phone, date, slot } = req.body;
         console.log(req.body);
+        MilitaryToStandard = {
+            "0000": { hour: 0, min: 0 },
+            "0030": { hour: 0, min: 30 },
+            "0100": { hour: 1, min: 0 },
+            "0130": { hour: 1, min: 30 },
+            "0200": { hour: 2, min: 0 },
+            "0230": { hour: 2, min: 30 },
+            "0300": { hour: 3, min: 0 },
+        }
         const appointment = new Appointment({
             name: name,
             email: email,
             phone: phone,
             slots: {
-                month: "Jan",
-                day: "1",
-                year: "2000",
-                hour: "12",
-                minute: "01"
+                month: date.months,
+                day: date.date,
+                year: date.years,
+                hour: MilitaryToStandard[slot].hour,
+                minute: MilitaryToStandard[slot].min
             },
             createdAt: Date.now(),
             _user: req.user.id
